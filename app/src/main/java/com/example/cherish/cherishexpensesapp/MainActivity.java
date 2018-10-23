c93a22669a924f02830bd7d330bf3d1d6e6801a1
ugokingsley <ugokingsley5@gmail.com>
2018-10-23 14:00:06 +0000

    update

--- app/src/main/java/com/example/cherish/cherishexpensesapp/MainActivity.java
+++ app/src/main/java/com/example/cherish/cherishexpensesapp/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.cherish.cherishexpensesapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.AsyncTaskLoader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.content.Loader;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.cherish.cherishexpensesapp.data.ExpenseContract;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Constants for logging and referring to a unique loader
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int TASK_LOADER_ID = 0;
+
+    // Member variables for the adapter and RecyclerView
+    private CustomCursorAdapter mAdapter;
+    RecyclerView mRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +37,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        // Set the RecyclerView to its corresponding view
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerViewExpenses);
+
+        // Set the layout for the RecyclerView to be a linear layout, which measures and
+        // positions items within a RecyclerView into a linear list
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the adapter and attach it to the RecyclerView
+        mAdapter = new CustomCursorAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        /*
+         a touch helper to the RecyclerView to recognize when a user swipes to delete an item.
+         An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+         and uses callbacks to signal when a user is performing these actions.
+         */
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            // Called when a user swipes left or right on a ViewHolder
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+
+
+                // Retrieve the id of the task to delete
+                int id = (int) viewHolder.itemView.getTag();
+
+                // Build appropriate uri with String row id appended
+                String stringId = Integer.toString(id);
+                Uri uri = ExpenseContract.ExpenseEntry.CONTENT_URI;
+                uri = uri.buildUpon().appendPath(stringId).build();
+
+                //  Delete a single row of data using a ContentResolver
+                getContentResolver().delete(uri, null, null);
+
+                //  Restart the loader to re-query for all tasks after a deletion
+                getSupportLoaderManager().restartLoader(TASK_LOADER_ID, null, MainActivity.this);
+
+            }
+        }).attachToRecyclerView(mRecyclerView);
+
+
 
         FloatingActionButton fabExpense = (FloatingActionButton) findViewById(R.id.fabExpense);
         FloatingActionButton fabIncome = (FloatingActionButton) findViewById(R.id.fabIncome);
@@ -39,6 +105,93 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(addIncomeIntent);
             }
         });
+
+        /*
+          a loader is initialized and active. If the loader doesn't already exist, one is
+         created, otherwise the last created loader is re-used.
+         */
+        getSupportLoaderManager().initLoader(TASK_LOADER_ID, null, this);
+    }
+
+    /**
+     * Instantiates and returns a new AsyncTaskLoader with the given ID.
+     * This loader will return expense data as a Cursor or null if an error occurs.
+     */
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, final Bundle loaderArgs) {
+
+        return new AsyncTaskLoader<Cursor>(this) {
+
+            // Initialize a Cursor, this will hold all the task data
+            Cursor mExpenseData = null;
+
+            // onStartLoading() is called when a loader first starts loading data
+            @Override
+            protected void onStartLoading() {
+                if (mExpenseData != null) {
+                    // Delivers any previously loaded data immediately
+                    deliverResult(mExpenseData);
+                } else {
+                    // Force a new load
+                    forceLoad();
+                }
+            }
+
+            // loadInBackground() performs asynchronous loading of data
+            @Override
+            public Cursor loadInBackground() {
+                // Will implement to load data
+
+                // Query and load all task data in the background; sort by priority
+                // [Hint] use a try/catch block to catch any errors in loading data
+
+                try {
+                    return getContentResolver().query(ExpenseContract.ExpenseEntry.CONTENT_URI,
+                            null,
+                            null,
+                            null,
+                            ExpenseContract.ExpenseEntry.COLUMN_DATE);
+
+                } catch (Exception e) {
+                    Log.e(TAG, "Failed to asynchronously load data.");
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            // deliverResult sends the result of the load, a Cursor, to the registered listener
+            public void deliverResult(Cursor data) {
+                mExpenseData = data;
+                super.deliverResult(data);
+            }
+        };
+
+    }
+
+
+    /**
+     * Called when a previously created loader has finished its load.
+     *
+     * @param loader The Loader that has finished.
+     * @param data The data generated by the Loader.
+     */
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update the data that the adapter uses to create ViewHolders
+        mAdapter.swapCursor(data);
+    }
+
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.
+     * onLoaderReset removes any references this activity had to the loader's data.
+     *
+     * @param loader The Loader that is being reset.
+     */
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
     }
 
     @Override
